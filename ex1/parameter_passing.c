381e3e28fa6979723a5553a44d858319d530b0ef
Camilla Rygh <camilla.rygh@asolvi.com>
2021-01-17 13:38:21 +0100

    Static local variables in nm

--- ex1/parameter_passing.c
+++ ex1/parameter_passing.c
@@ -9,6 +9,10 @@ void increment_with_value(int a, int b)
 
 int main(void)
 {
+  int local_uninit;
+  int local_init = 10;
+  static int static_local_uninit;
+  static int static_local_init = 5;
   //increment_with_value(a, 1);
   printf("%i", a);
   //return a;
