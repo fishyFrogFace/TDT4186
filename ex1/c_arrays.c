13b7a330037cc4687c297067db2b1b9e35dbaab3
Camilla Rygh <camilla.rygh@asolvi.com>
2021-01-19 08:25:37 +0100

    Add static to foo in stack smashing

--- ex1/c_arrays.c
+++ ex1/c_arrays.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+void original(void)
 {
   int foo = 0;
   char s[12];
   char *t = "01234567890123";
   printf("foo %p\n  s %p\n", &foo, s);
-  // to allow stack smashing: gcc -Wall -fno-stack-protector -o c_arrays.o ex1/c_arrays.c
   strcpy(s, t);
   printf("foo = %d\n", foo);
+}
+
+void modified(void)
+{
+  static int foo = 0;
+  char s[12];
+  char *t = "01234567890123";
+  printf("foo %p\n  s %p\n", &foo, s);
+  strcpy(s, t);
+  printf("foo = %d\n", foo);
+}
+
+// to allow stack smashing: gcc -Wall -fno-stack-protector -o c_arrays.o ex1/c_arrays.c
+int main(void)
+{
+  printf("Original code:\n");
+  original();
+  printf("\nModified with static variable:\n");
+  modified();
+  return 0;
 }
