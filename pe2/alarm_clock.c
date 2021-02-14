e0bba35dd3b7c09a6de227f5defde41695834c01
Camilla Rygh <camilla.rygh@asolvi.com>
2021-02-14 23:14:59 +0100

    Single alarm clock

--- pe2/alarm_clock.c
+++ pe2/alarm_clock.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
+
+void set_alarm(int seconds)
+{
+  sleep(seconds);
+  printf("\a");
+  printf("Alarm going off!\n");
+}
 
 int main()
 {
@@ -6,4 +14,6 @@ int main()
   printf("How many seconds do you want to set the timer to? ");
   scanf("%d", &seconds);
   printf("You set the timer to ring in %i second(s)\n", seconds);
+  set_alarm(seconds);
+  return 0;
 }
