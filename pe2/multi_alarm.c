3ad82c3aaebd3e9b8a1f6843373fe4a04d7040ff
Camilla Rygh <camillahallo@mail.com>
2021-02-17 21:22:54 +0100

    A stab at a multialarm

---
+++ pe2/multi_alarm.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void timer(int seconds)
+{
+  sleep(seconds);
+  printf("\a");
+  printf("\nRING! child process %d rang after %d seconds\n", getpid(), seconds);
+  exit(0);
+}
+
+int get_user_input(void)
+{
+  printf("\nEnter a delay in seconds: ");
+  int seconds;
+  int thing = scanf("%d", &seconds);
+  printf("thing: %d", thing);
+  return seconds;
+}
+
+int main(void)
+{
+  int wstatus;
+  while (1)
+  {
+    int seconds = get_user_input();
+    int terminated = waitpid(-1, &wstatus, WNOHANG);
+    if (terminated != 0 && terminated != -1)
+      printf("\nChild process %d terminated", terminated);
+    int cpid = fork();
+    if (cpid == 0)
+    {
+      timer(seconds);
+    }
+    else
+    {
+      printf("\nStarted child process with pid %d\n", cpid);
+    }
+  }
+
+  return 0;
+}
