6a5257d56c9dc1d4a17c2998858e4704d1fcba5b
Camilla Rygh <camilla.rygh@asolvi.com>
2021-02-19 11:35:28 +0100

    More error handling

--- pe2/error_handling.c
+++ pe2/error_handling.c
@@ -10,7 +10,7 @@ void timer(int seconds)
 {
   sleep(seconds);
   printf("\a");
-  printf("\nRING! child process %d rang after %d seconds\n", getpid(), seconds);
+  printf("\nRING! child process %d rang after %d seconds\nEnter a delay in seconds: ", getpid(), seconds);
   exit(0);
 }
 
@@ -19,11 +19,12 @@ int get_user_input(void)
   printf("\nEnter a delay in seconds: ");
   errno = 0;
   long seconds;
-  char *user_input;
-  int num_parsed_inputs = scanf("%s", user_input);
-  seconds = strtol(user_input, NULL, 10);
 
-  printf("Now printing seconds: %d\n", seconds);
+  // write programs to handle text streams,
+  // because that is an universal interface
+  char *user_input;
+  scanf("%s", user_input);
+  seconds = strtol(user_input, NULL, 10);
 
   if (errno != 0)
   {
@@ -44,15 +45,32 @@ int main(void)
   int wstatus;
   while (1)
   {
+    errno = 0;
     int seconds = get_user_input();
 
-    int terminated = waitpid(-1, &wstatus, WNOHANG);
-    if (terminated != 0 && terminated != -1)
-      printf("\nChild process %d terminated", terminated);
+    int terminated;
+    while ((terminated = waitpid(-1, &wstatus, WNOHANG)) && terminated != 0 && terminated != -1)
+    {
+      printf("\nChild process %d terminated\n", terminated);
+    }
+
+    // check if there was an error when calling waitpid
+    if (terminated == -1 && errno != ECHILD)
+    {
+      perror("waitpid");
+      exit(EXIT_FAILURE);
+    }
+
     int cpid = fork();
     if (cpid == 0)
     {
       timer(seconds);
+    }
+    // if the fork failed for some reason, exit the program
+    else if (cpid == -1)
+    {
+      perror("fork");
+      exit(EXIT_FAILURE);
     }
     else
     {
