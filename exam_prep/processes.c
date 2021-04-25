085a2aab03d38e944c8d8dcdd10a5dcf9bedce20
Camilla Rygh <camilla.rygh@asolvi.com>
2021-04-25 15:27:50 +0200

    Fix 4/5 mistakes

--- exam_prep/processes.c
+++ exam_prep/processes.c
@@ -38,14 +38,17 @@ void task_3() {
   int pipe1[2], pipe2[2]; // pipe2 should have two elements
   pipe(pipe1);
   pipe(pipe2);
+
   if (fork() == 0) {
     dup2(pipe1[WRITE], STDOUT_FILENO);
     dup2(pipe2[READ],
          STDIN_FILENO); // switched these two, so we don't overwrite pipe2[READ]
     close(pipe1[READ]);
-    close(pipe1[WRITE]);
+    close(pipe2[WRITE]); // change to pipe2 here, or else we close the whole
+                         // pipe for the parent process
     exec(A);
   }
+
   if (fork() != 0) {
     dup2(pipe1[READ], STDIN_FILENO);
     dup2(pipe2[WRITE], STDOUT_FILENO);
@@ -53,6 +56,7 @@ void task_3() {
     close(pipe2[READ]);
     exec(B);
   }
+
   close(pipe1[READ]);
   close(pipe1[WRITE]);
   close(pipe2[READ]);
